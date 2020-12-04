8fe2eac3017d478f5c1a28412a83a5fbd8dd3924
antonpanchishko <PanchishkoMayor@gmail.com>
2020-12-04 11:25:15 +0200

    1) hw solution (v4 User-score)

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,19 +5,13 @@ import core.basesyntax.exception.UserNotFoundException;
 public class UserService {
 
     public int getUserScore(String[] records, String email) {
-        String score = "";
         Boolean check = false;
         for (int i = 0; i < records.length; i++) {
             String [] splitRecord = records[i].split(":");
-            score = splitRecord[1];
             if (email.equals(splitRecord[0])) {
-                check = true;
-                return Integer.parseInt(score);
+                return Integer.parseInt(splitRecord[1]);
             }
         }
-        if (check == false) {
-            throw new UserNotFoundException("User with given email doesn't exist");
-        }
-        return 0;
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
